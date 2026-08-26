2c2f41e378614cfd76131503c478772d8fc3ec90
micheladafriaa-sys <micheladafriaa@gmail.com>
2026-08-25 20:52:04 -0600

    Update pets.js

--- data/pets.js
+++ data/pets.js
@@ -6,6 +6,40 @@ const pets = [
     demand: "high",
     trend: "stable",
     neon: true,
-    mega: true
+    mega: true,
+    potionException: true
+  },
+
+  {
+    name: "Shadow Dragon",
+    value: 850,
+    rarity: "Legendary",
+    demand: "high",
+    trend: "stable",
+    neon: true,
+    mega: true,
+    potionException: true
+  },
+
+  {
+    name: "Giraffe",
+    value: 475,
+    rarity: "Legendary",
+    demand: "high",
+    trend: "stable",
+    neon: true,
+    mega: true,
+    potionException: true
+  },
+
+  {
+    name: "Frost Dragon",
+    value: 326,
+    rarity: "Legendary",
+    demand: "high",
+    trend: "stable",
+    neon: true,
+    mega: true,
+    potionException: true
   }
 ];
